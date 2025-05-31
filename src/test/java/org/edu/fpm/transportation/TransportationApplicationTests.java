6dec0d101691ef42ebd1711d1ecc9db96ebc2df1
Ivan4112 <ivanbondar224@gmail.com>
2025-05-31 16:35:52 +0300

    fix test

--- src/test/java/org/edu/fpm/transportation/TransportationApplicationTests.java
+++ src/test/java/org/edu/fpm/transportation/TransportationApplicationTests.java
@@ -3,7 +3,6 @@ package org.edu.fpm.transportation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TransportationApplicationTests {
 
     @Test
