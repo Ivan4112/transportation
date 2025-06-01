73caf01c6a0823927ddd044028c13ce19cdc7706
Ivan4112 <ivanbondar224@gmail.com>
2025-06-01 19:55:17 +0300

    changed AuthController

--- src/main/java/org/edu/fpm/transportation/controller/AuthController.java
+++ src/main/java/org/edu/fpm/transportation/controller/AuthController.java
@@ -1,31 +1,30 @@
 package org.edu.fpm.transportation.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.edu.fpm.transportation.dto.auth.AuthResponseDto;
 import org.edu.fpm.transportation.dto.auth.signin.SignInRequestDto;
 import org.edu.fpm.transportation.dto.auth.signup.SignUpRequestDto;
 import org.edu.fpm.transportation.service.security.AuthService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
     private final AuthService authService;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
+    @PostMapping("/sign-in")
+    public ResponseEntity<AuthResponseDto> signIn(@RequestBody SignInRequestDto request) {
+        return ResponseEntity.ok(authService.signIn(request));
     }
 
     @PostMapping("/sign-up")
-    @ResponseStatus(HttpStatus.CREATED)
-    public AuthResponseDto registerUser(@RequestBody SignUpRequestDto signUpRequestDto) {
-        return authService.signUp(signUpRequestDto);
-    }
-
-
-    @PostMapping("/sign-in")
-    @ResponseStatus(HttpStatus.CREATED)
-    public AuthResponseDto loginUser(@RequestBody SignInRequestDto signInRequestDto) {
-        return authService.login(signInRequestDto);
+    public ResponseEntity<AuthResponseDto> signUp(@RequestBody SignUpRequestDto request) {
+        return ResponseEntity.ok(authService.signUp(request));
     }
 }
