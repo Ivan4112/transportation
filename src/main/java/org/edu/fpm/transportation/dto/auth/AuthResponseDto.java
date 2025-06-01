798a1dcc45f781270197c8701ebcbe69ca8d8e3a
Ivan4112 <ivanbondar224@gmail.com>
2025-06-01 19:55:24 +0300

    changed AuthResponseDto

--- src/main/java/org/edu/fpm/transportation/dto/auth/AuthResponseDto.java
+++ src/main/java/org/edu/fpm/transportation/dto/auth/AuthResponseDto.java
@@ -5,14 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponseDto {
     private String accessToken;
     private String role;
-    private int userId;
+    private Integer userId;
     private String firstName;
     private String lastName;
     private String email;
