d2273cbae06237beb6e3ed5872a5fe1e458426e2
Ivan4112 <ivanbondar224@gmail.com>
2025-06-02 03:19:11 +0300

    added new repository for driver

--- src/main/java/org/edu/fpm/transportation/repository/VehicleRepository.java
+++ src/main/java/org/edu/fpm/transportation/repository/VehicleRepository.java
@@ -1,7 +1,11 @@
 package org.edu.fpm.transportation.repository;
 
+import org.edu.fpm.transportation.entity.User;
 import org.edu.fpm.transportation.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+    Optional<Vehicle> findByDriver(User driver);
 }
